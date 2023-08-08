f9d5e7354486d00f60ddc2c8c9bfb58c7852c2d0
Larisa Stupina <ls@esurance.ch>
2023-08-08 09:34:21 +0300

    Function homework

--- tasks/cycles/cycles.js
+++ tasks/cycles/cycles.js
@@ -18,7 +18,7 @@ for (let i = 1; i < n + 1; i++) {
 
 whileFactorial = 1;
 let i = 1;
-while (i < n + 1) {
+while (i < n + 1) { 
   whileFactorial = whileFactorial * i;
   i++;
 }
--- tasks/functions/functions.js
+++ tasks/functions/functions.js
@@ -3,7 +3,7 @@
  *
  */
 function sum(a, b) {
-	
+	return (a + b)
 }
 
 /**
