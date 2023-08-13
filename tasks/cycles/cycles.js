d5e98d7d08f38f811cd3c0d1183fc65a03013bee
Denys Lepekha <dlepekha@esurance.ch>
2023-08-13 20:44:48 +0300

    Added few more loops tasks

--- tasks/cycles/cycles.js
+++ tasks/cycles/cycles.js
@@ -37,3 +37,43 @@ module.exports = {
   str,
   totalIncome
 };
+
+
+// Additional loop tasks
+
+/** TODO
+ * Write a loop that prints even numbers from 2 to 20.
+ * Solve task using for, while and do while loops
+ */
+
+
+/** TODO
+ * Write a loop that prints odd numbers from 1 to 15.
+ * Solve task using for, while and do while loops
+ */
+
+/** TODO
+ * Given an array of numbers, calculate and print the sum of all the numbers.
+ */
+
+const numbers = [3, 7, 2, 9, 5, 99, 0, 1111, 7738];
+
+/** TODO
+ * Given an array of numbers, find and print the maximum value.
+ */
+
+const numbers2 = [45, 67, 23, 89, 12];
+
+
+/** TODO
+ * Given an array of numbers, multiple by 5 each array element.
+ */
+
+const numbers3 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+/** TODO
+ * Iterate through all numbers from 1 to 45. Print the following:
+ * For multiples of 3 print “Fizz”
+ * For multiples of 5 print “Buzz”
+ * For multiples of 3 and 5 print “FizzBuzz”
+ */
