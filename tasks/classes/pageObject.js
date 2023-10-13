ae7ebdd055c0606741dfaf52c6b95070d44fb95f
Denys Lepekha <dlepekha@esurance.ch>
2023-10-13 15:50:50 +0300

    First e2e task

--- tasks/classes/pageObject.js
+++ tasks/classes/pageObject.js
@@ -1,20 +1,17 @@
 /**
- * Organize a page object structure https://drive.google.com/file/d/1w3B4Z2ABy2AkacJFRlQosrYXwwv3jw_V/view
+ * 1. Install Playwright: https://playwright.dev/docs/intro#installing-playwright
+ * 2. After installation check version using following command: npx playwright --version
+ * 3. Inside the ./tasks/classes/test folder create a file with the name first-e2e-test.spec.js
+ * 4. Using Playwright documentation create a test scenario that executes:
+ *    1) Open https://app.dev-esurance.ch/gastrosuisse
+ *    2) Select any product and continue to the product page
+ *    3) Select any price parameters if needed and proceed to the product config page
+ *    4) On the product config page activate any additional coverage and add product to the basket
+ *    5) Retrieve product price value and save to the variable
+ *    5) Verify that product are added to the basket (check product name)
+ *    6) Verify that prices in the basket and product config page are exactly the same
+ *    7) Install VS code extension Playwright Test for VSCode
+ *    8) Run you test and make sure it's passed
+ *    9) Also run test from terminal using command npx playwright test first-e2e-test.spec.js
+ *    10) Run command npx playwright show-report to see test run report
  */
-
-class BasePage {
-	
-}
-
-class LoginPage {
-
-}
-
-class Component {
-	
-}
-
-module.exports = {
-	BasePage,
-	LoginPage
-};
