e5fd3d6e0b95ada7664f65cbd30b92b9ad888e4f
Larisa Stupina <ls@esurance.ch>
2023-10-20 17:51:24 +0300

    First Gasto test

--- tasks/classes/pageObject.js
+++ tasks/classes/pageObject.js
@@ -1,7 +1,7 @@
 /**
  * 1. Install Playwright: https://playwright.dev/docs/intro#installing-playwright
  * 2. After installation check version using following command: npx playwright --version
- * 3. Inside the ./tasks/classes/test folder create a file with the name first-e2e-test.spec.js
+ * 3. Inside the tests folder create a file with the name first-e2e-test.spec.ts
  * 4. Using Playwright documentation create a test scenario that executes:
  *    1) Open https://app.dev-esurance.ch/gastrosuisse
  *    2) Select any product and continue to the product page
@@ -12,6 +12,6 @@
  *    6) Verify that prices in the basket and product config page are exactly the same
  *    7) Install VS code extension Playwright Test for VSCode
  *    8) Run you test and make sure it's passed
- *    9) Also run test from terminal using command npx playwright test first-e2e-test.spec.js
+ *    9) Also run test from terminal using command npx playwright test first-e2e-test.spec.ts
  *    10) Run command npx playwright show-report to see test run report
  */
